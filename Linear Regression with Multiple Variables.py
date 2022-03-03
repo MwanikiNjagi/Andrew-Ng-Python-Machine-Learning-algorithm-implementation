a54305964480c1f53cb55454f8e6e0240e2429fe
MwanikiNjagi <46248187+MwanikiNjagi@users.noreply.github.com>
2022-03-03 19:36:21 +0300

    Update Linear Regression with Multiple Variables.py

--- Linear Regression with Multiple Variables.py
+++ Linear Regression with Multiple Variables.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd 
 
-data = pd.read_csv('C:\ex1data2.txt', sep = ',', header = None)
+data = pd.read_csv('C:\ex1data2.txt', sep = ',', header = None)#Input name of the file and location as in your computer
 X = data.iloc[:,0:2] #reads first two columns
 Y = data.iloc[:,2] #reads the third column
 m = len(Y)
